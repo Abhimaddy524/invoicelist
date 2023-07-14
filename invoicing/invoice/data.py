fab8bed276dc0197805466d6019a51cbfb67a6b4
Abhimaddy524 <114563289+Abhimaddy524@users.noreply.github.com>
2023-07-14 23:47:30 +0530

    Add files via upload

---
+++ invoicing/invoice/data.py
@@ -0,0 +1,22 @@
+invoicesData=[
+    {
+    "invoice_id":1,
+    "client_name":"Abhishek",
+    "date":"2023-05-30",
+    "items":[
+        {
+        "desc":"product 1",
+        "rate":100,
+        "quantity":2
+    }
+    ],
+}
+]
+userData=[
+    {
+    "user_id":1,
+    "name":"Abhishek",
+    "email":"abhishekdas8826@gmail.com",
+    "password":"123456",
+}
+]
